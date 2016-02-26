8157e96dd6c7ec818180bfe93b5ea78fe6ffcab8
Ouyang Yingjie <ouyang_yingjie@outlook.com>
2016-02-26 16:37:13 +0800

    ref:printInventory.js refactoring function

--- pos_v1/main/printInventory.js
+++ pos_v1/main/printInventory.js
@@ -3,28 +3,26 @@ function printInventory(receiptItems){
   var split_line = '----------------------\n';
   var star_line = '**********************';
 
-  var final_money = 0;
+  var totalPrice = 0;
   var total_save = 0;
   var print_content = "";
+
   print_content += first_line;
 
   receiptItems.forEach(function(receipt_item){
-    final_money += receipt_item.total - receipt_item.save;
+    totalPrice += receipt_item.total - receipt_item.save;
     total_save += receipt_item.save;
-  });
-
-  var final_money_line = "总计：" + final_money.toFixed(2) + "(元)\n";
-  var total_save_line = "节省：" + total_save.toFixed(2) + "(元)\n";
-  receiptItems.forEach(function(receipt_item){
     var print_pattern = "名称：" + receipt_item.cartItem.item.name + "，" + "数量：" + receipt_item.cartItem.amount + receipt_item.cartItem.item.unit + "，单价：" + receipt_item.cartItem.item.price.toFixed(2) + "(元)，小计：" + (receipt_item.total - receipt_item.save).toFixed(2) + "(元)\n";
     print_content += print_pattern;
   });
+
+  var final_money_line = "总计：" + totalPrice.toFixed(2) + "(元)\n";
+  var total_save_line = "节省：" + total_save.toFixed(2) + "(元)\n";
 
   print_content += split_line;
   print_content += final_money_line;
   print_content += total_save_line;
   print_content += star_line;
-
 
   console.log(print_content);
 }
